4ace553a86c6b087b26b62db917e13878b4c3dc1
lucas-lubanco <lucas_lubanco@hotmail.com>
2020-06-14 10:00:45 -0300

    Integração front end socke.io

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const formEmail = document.querySelector('#formEmail');
 const formTelefone = document.querySelector('#formTelefone');
 const formMessage = document.querySelector('#formMessage');
 const formSubm = document.querySelector('#formSubm');
+const socket = io('localhost:3000');
+let user;
+
+function getData(){
+    user = {
+        nome: formName.value,
+        email: formEmail.value,
+        telefone: formTelefone.value,
+        message: formMessage.value,
+    };
+}
 
 formSubm.addEventListener('click', e=> {
     e.preventDefault();
-    const form = {
-        nome: formName,
-        email: formEmail,
-        telefone: formTelefone,
-        message: formMessage,
-    };
-}, false);
+    socket.on('connect', function(){
+        getData();
+        socket.emit('sendForm', user)
+    })
 
+}, false);
